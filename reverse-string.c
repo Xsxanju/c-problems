432e31b84f13ab2229f40288cc9168b59e4e5784
Xsxanju <sanjaysenthilkumar@gmail.com>
2024-12-21 21:00:22 +0530

    Update reverse-string.c

--- reverse-string.c
+++ reverse-string.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 
 void reverseString(char* s, int sSize) {
     int left = 0;
     int right = sSize - 1;
 
     while (left < right) {
-        // Swap characters at left and right pointers
+        // Swap the characters at the two pointers
         char temp = s[left];
         s[left] = s[right];
         s[right] = temp;
 
-        // Move pointers towards the center
+        // Move the pointers toward the center
         left++;
         right--;
     }
-}
-
-int main() {
-    char str[] = "hello";
-    int len = strlen(str);
-
-    reverseString(str, len);
-
-    printf("Reversed string: %s\n", str);
-
-    return 0;
 }
